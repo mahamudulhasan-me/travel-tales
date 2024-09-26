b04ddd03ce07749e3db6e4a7354a1e0f91dbd1c1
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2024-09-26 19:45:14 +0600

    end of module 40

--- Next.js/FoundX-Client/src/services/recentPosts/index.ts
+++ Next.js/FoundX-Client/src/services/recentPosts/index.ts
@@ -1,10 +1,14 @@
 /* eslint-disable prettier/prettier */
+
 import envConfig from "@/src/config/envConfig";
+import delay from "@/src/utils/dely";
 
 export const getRecentPosts = async () => {
   const res = await fetch(
     `${envConfig.baseUrl}/items?sortBy=-createdAt&limit=9`
   );
+
+  await delay(5000);
 
   return res.json();
 };
---
+++ Next.js/FoundX-Client/src/utils/dely.ts
@@ -0,0 +1,5 @@
+const delay = async (ms = 2000) => {
+  return await new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+export default delay;
