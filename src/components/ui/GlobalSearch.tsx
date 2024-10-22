0809e9be7a22099370d642906b59bc3122abc55b
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2024-10-22 21:01:23 +0600

    add search modal

--- src/components/ui/GlobalSearch.tsx
+++ src/components/ui/GlobalSearch.tsx
@@ -1,7 +1,7 @@
 import "./GlobalSearchStyle.css";
 const GlobalSearch = () => {
   return (
-    <>
+    <div className="relative">
       <div className="group">
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -18,9 +18,21 @@ const GlobalSearch = () => {
           <circle cx="11" cy="11" r="8" />
           <path d="m21 21-4.3-4.3" />
         </svg>
-        <input placeholder="Search..." type="search" className="input" />
+        <input placeholder="Search Post..." type="search" className="input" />
       </div>
-    </>
+      <div className="absolute w-72 bg-white p-2 glassmorphisom rounded-b-md">
+        <h6 className="font-semibold">Mahamudul Hasan </h6>
+        <article>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ea
+        </article>
+        <div className="flex items-center text-sm">
+          <button className="text-primary font-semibold w-24">
+            View Details
+          </button>
+          <div className="w-full h-[1px] bg-gray-500"></div>
+        </div>
+      </div>
+    </div>
   );
 };
 
